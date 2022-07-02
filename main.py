ce29bb6730a50353d782bf52cb38518b8904c504
pooya sharifi <sharifipooya@gmail.com>
2022-07-02 21:05:35 +0430

    alignment logos fixed ,open canceling fixed

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ matplotlib.use("Qt5Agg")
 from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
 from matplotlib.backends.backend_qt5agg import NavigationToolbar2QT as NavigationToolbar
 from matplotlib.figure import Figure
-from PyQt5 import uic, QtCore,QtGui
+from PyQt5 import uic, QtCore, QtGui
 from PyQt5.QtWidgets import (
     QApplication,
     QMainWindow,
@@ -32,7 +32,7 @@ from PyQt5.QtWidgets import (
     QFontDialog,
     QTextEdit,
 )
-from PyQt5.QtGui import QFontDatabase, QFont, QKeySequence,QIcon
+from PyQt5.QtGui import QFontDatabase, QFont, QKeySequence, QIcon
 import numpy as np
 from time import sleep
 
@@ -43,8 +43,8 @@ class MainWindow(QMainWindow, Form):
     def __init__(self, *args, **kwargs):
         super(MainWindow, self).__init__(*args, **kwargs)
         self.flag_bold = 0
-        self.flag_italic=0
-        self.flag_underline=0
+        self.flag_italic = 0
+        self.flag_underline = 0
         self.setupUi(self)
         self.setWindowTitle("Text editor")
         self.setStyleSheet("background-color: none;")
@@ -52,11 +52,11 @@ class MainWindow(QMainWindow, Form):
         # self.editor = QPlainTextEdit()  # Could also use a QTextEdit and set self.editor.setAcceptRichText(False)
         # self.setCentralWidget(self.editor)
         # setting icon
-        self.leftAlignButton.setIcon(QIcon('left_align.png'))
-        self.rightAlignButton.setIcon(QIcon('right_align.png'))
-        self.centerAlignButton.setIcon(QIcon('center_align.png'))
+        self.leftAlignButton.setIcon(QIcon("./Resources/left_align.png"))
+        self.rightAlignButton.setIcon(QIcon("./Resources/right_align.png"))
+        self.centerAlignButton.setIcon(QIcon("./Resources/center_align.png"))
         # Setup the QTextEdit editor configuration
-        
+
         # self.editor.setFont(fixedfont)
         self.NewButton.clicked.connect(self.Newpage)
         self.BoldButton.clicked.connect(self.Bold)
@@ -70,12 +70,11 @@ class MainWindow(QMainWindow, Form):
         self.ColorButton.clicked.connect(self.text_color)
         self.FontButton.clicked.connect(self.text_font)
         self.spinBox.valueChanged.connect(self.font_size)
-        
+
         # getting current value
         self.spinBox.setValue(8)
         self.spinBox.valueChanged.connect(self.Font_change)
-        
-        
+
         # self.redoButton.clicked.connect(self.Redo_text)
 
         self.BoldButton.setStyleSheet("background-color : white")
@@ -88,29 +87,28 @@ class MainWindow(QMainWindow, Form):
         self.ColorButton.setStyleSheet("background-color : white")
         self.FontButton.setStyleSheet("background-color : white")
         self.spinBox.setStyleSheet("background-color : white")
-        
+
         # shortcuts
-        self.open_new_file_shortcut = QShortcut(QKeySequence('Ctrl+O'), self)
+        self.open_new_file_shortcut = QShortcut(QKeySequence("Ctrl+O"), self)
         self.open_new_file_shortcut.activated.connect(self.open_dialog_box)
 
-        self.save_file_shortcut = QShortcut(QKeySequence('Ctrl+S'), self)
+        self.save_file_shortcut = QShortcut(QKeySequence("Ctrl+S"), self)
         self.save_file_shortcut.activated.connect(self.Save__File)
 
-        self.italic_text_shortcut = QShortcut(QKeySequence('Ctrl+I'), self)
+        self.italic_text_shortcut = QShortcut(QKeySequence("Ctrl+I"), self)
         self.italic_text_shortcut.activated.connect(self.italic)
 
-        self.bold_text_shortcut = QShortcut(QKeySequence('Ctrl+B'), self)
+        self.bold_text_shortcut = QShortcut(QKeySequence("Ctrl+B"), self)
         self.bold_text_shortcut.activated.connect(self.Bold)
 
-        self.underline_text_shortcut = QShortcut(QKeySequence('Ctrl+U'), self)
+        self.underline_text_shortcut = QShortcut(QKeySequence("Ctrl+U"), self)
         self.underline_text_shortcut.activated.connect(self.underline)
 
-
-####################################################
+        ####################################################
         edit_toolbar = QToolBar("Edit")
         self.addToolBar(edit_toolbar)
         edit_menu = self.menuBar().addMenu("&Edit")
-####################################################
+        ####################################################
         # undo action
         undo_action = QAction("UNDO", self)
         undo_action.setStatusTip("Undo last change")
@@ -125,27 +123,27 @@ class MainWindow(QMainWindow, Form):
         edit_toolbar.addAction(redo_action)
         edit_menu.addAction(redo_action)
 
-       # cut action
+        # cut action
         cut_action = QAction("Cut", self)
         cut_action.setStatusTip("Cut selected text")
         cut_action.triggered.connect(self.textEdit.cut)
         edit_toolbar.addAction(cut_action)
         edit_menu.addAction(cut_action)
- 
+
         # copy action
         copy_action = QAction("Copy", self)
         copy_action.setStatusTip("Copy selected text")
         copy_action.triggered.connect(self.textEdit.copy)
         edit_toolbar.addAction(copy_action)
         edit_menu.addAction(copy_action)
- 
+
         # paste action
         paste_action = QAction("Paste", self)
         paste_action.setStatusTip("Paste from clipboard")
         paste_action.triggered.connect(self.textEdit.paste)
         edit_toolbar.addAction(paste_action)
         edit_menu.addAction(paste_action)
- 
+
         # select all action
         select_action = QAction("Select all", self)
         select_action.setStatusTip("Select all text")
@@ -153,24 +151,24 @@ class MainWindow(QMainWindow, Form):
         edit_toolbar.addAction(select_action)
         edit_menu.addAction(select_action)
 
-
         # fekr konam font ro darim okay mikonim inja
         fixedFont = QFontDatabase.systemFont(QFontDatabase.FixedFont)
         fixedFont.setPointSize(5000000)
         self.editor = QPlainTextEdit()
         self.editor.setFont(fixedFont)
+
     def underline(self):
         print("underline")
         # self.textEdit.setFontUnderline(True)
         print(self.flag_underline)
-        if (self.flag_underline==0):
+        if self.flag_underline == 0:
             self.textEdit.setFontUnderline(True)
-            self.flag_underline=1
+            self.flag_underline = 1
             print(self.flag_underline)
         else:
             print(self.flag_underline)
             self.textEdit.setFontUnderline(False)
-            self.flag_underline=0
+            self.flag_underline = 0
             print(self.flag_underline)
 
     def Newpage(self, event):
@@ -178,13 +176,18 @@ class MainWindow(QMainWindow, Form):
         messageBox = QMessageBox()
         title = "New page?"
         message = "WARNING !!\n\nIf you quit without saving, any changes made to the file will be lost.\n\nSave file before quiting?"
-    
-        reply = messageBox.question(self, title, message, messageBox.Yes | messageBox.No |
-                messageBox.Cancel, messageBox.Cancel)
+
+        reply = messageBox.question(
+            self,
+            title,
+            message,
+            messageBox.Yes | messageBox.No | messageBox.Cancel,
+            messageBox.Cancel,
+        )
         if reply == messageBox.Yes:
             return_value = self.Save__File()
             if return_value == False:
-                event.ignore()  
+                event.ignore()
         elif reply == messageBox.No:
             self.textEdit.clear()
         else:
@@ -194,26 +197,27 @@ class MainWindow(QMainWindow, Form):
     def Bold(self):
         print("Bold clicked")
         print(self.flag_bold)
-        if (self.flag_bold==0):
+        if self.flag_bold == 0:
             self.textEdit.setFontWeight(QFont.Bold)
-            self.flag_bold=1
+            self.flag_bold = 1
             print(self.flag_bold)
         else:
             print(self.flag_bold)
             self.textEdit.setFontWeight(QFont.Normal)
-            self.flag_bold=0
+            self.flag_bold = 0
             print(self.flag_bold)
+
     def italic(self):
         print("italic clicked")
         print(self.flag_italic)
-        if (self.flag_italic==0):
+        if self.flag_italic == 0:
             self.textEdit.setFontItalic(True)
-            self.flag_italic=1
+            self.flag_italic = 1
             print(self.flag_italic)
         else:
             print(self.flag_italic)
             self.textEdit.setFontItalic(False)
-            self.flag_italic=0
+            self.flag_italic = 0
             print(self.flag_italic)
 
     def open_dialog_box(self):
@@ -226,29 +230,32 @@ class MainWindow(QMainWindow, Form):
         #     with in__data:
         #         Text__ = in__data.read()
         #         self.textEdit.setText(Text__)
-        with open(path,"r") as f:
-            # print(f.readline())
-            _text = f.readline()
-            self.textEdit.setText(_text)
+        if path:
+            with open(path, "r") as f:
+                # print(f.readline())
+                # _text = f.readline()
+                _text=f.read()
+                self.textEdit.setText(_text)
 
     def Save__File(self):
         # S_File will get the directory path and extension.
-        S__File = QFileDialog.getSaveFileName(None,'SaveTextFile','/', "Text Files (*.txt)")
-    
+        S__File = QFileDialog.getSaveFileName(
+            None, "SaveTextFile", "/", "Text Files (*.txt)"
+        )
+
         # This will let you access the test in your QTextEdit
         Text = self.textEdit.toPlainText()
-    
+
         # This will prevent you from an error if pressed cancel on file dialog.
-        if S__File[0]: 
-        # Finally this will Save your file to the path selected.
-            with open(S__File[0], 'w') as file:
+        if S__File[0]:
+            # Finally this will Save your file to the path selected.
+            with open(S__File[0], "w") as file:
                 file.write(Text)
-    
+
     def Font_change(self):
         value = self.spinBox.value()
         print(value)
         self.textEdit.setFontPointSize(value)
-        
 
     def closeEvent(self, event):
         # if self.textEdit.empty() == 1:
@@ -257,18 +264,23 @@ class MainWindow(QMainWindow, Form):
         messageBox = QMessageBox()
         title = "Quit Application?"
         message = "WARNING !!\n\nIf you quit without saving, any changes made to the file will be lost.\n\nSave file before quitting?"
-    
-        reply = messageBox.question(self, title, message, messageBox.Yes | messageBox.No |
-                messageBox.Cancel, messageBox.Cancel)
+
+        reply = messageBox.question(
+            self,
+            title,
+            message,
+            messageBox.Yes | messageBox.No | messageBox.Cancel,
+            messageBox.Cancel,
+        )
         if reply == messageBox.Yes:
             return_value = self.Save__File()
             if return_value == False:
-                event.ignore()  
+                event.ignore()
         elif reply == messageBox.No:
             event.accept()
         else:
             event.ignore()
-    
+
     def text_color(self):
         color = QColorDialog.getColor()
         self.textEdit.setTextColor(color)
@@ -283,31 +295,17 @@ class MainWindow(QMainWindow, Form):
         # pass
         val = self.spinBox.value()
         self.textEdit.setFontPointSize(val)
-    
+
     # alignments
     def center_align(self):
         self.textEdit.setAlignment(QtCore.Qt.AlignCenter)
+
     def lef_align(self):
         self.textEdit.setAlignment(QtCore.Qt.AlignLeft)
+
     def rig_align(self):
         self.textEdit.setAlignment(QtCore.Qt.AlignRight)
 
-# class PlotThread(QtCore.QThread):
-#     update_trigger = QtCore.pyqtSignal(np.ndarray, np.ndarray)
-#     finished_trigger = QtCore.pyqtSignal()
-
-#     def __init__(self, window, decay):
-#         QtCore.QThread.__init__(self, parent=window)
-#         self.decay = decay
-#         self.window = window
-
-#     def run(self):
-#         x = np.linspace(0, 2 * np.pi, 1000)
-#         for n in range(20):
-#             y = np.cos(n * x) * np.exp(-self.decay * x)
-#             self.update_trigger.emit(x, y)
-#             sleep(0.1)
-#         self.finished_trigger.emit()
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
